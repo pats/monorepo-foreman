4b2d62be505e396149ab17ce45ecd294bfd89e5d
Patrycjusz Szydlo <patrycjusz.szydlo@gmail.com>
2022-08-22 15:36:32 +0200

    Some changes

--- apps/hello-world/src/index.js
+++ apps/hello-world/src/index.js
@@ -1,7 +1,9 @@
 const { iAmUiLibrary } = require('@monorepo-foreman/awesome-library');
-
+const { getAmazingThings } = require('@monorepo-foreman/amazing-things');
 
 (() => {
     const value = iAmUiLibrary();
     console.log(value);
+
+    console.log('AT', getAmazingThings());
 })();
