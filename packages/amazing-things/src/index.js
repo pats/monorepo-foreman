02c14cccab18b3a823f3abfc353d9e905b083fac
Patrycjusz Szydlo <patrycjusz.szydlo@gmail.com>
2023-01-02 15:19:19 +0100

    Some changes

--- packages/amazing-things/src/index.js
+++ packages/amazing-things/src/index.js
@@ -1,7 +1,7 @@
 const {iAmUiLibrary} = require('@monorepo-foreman/awesome-library');
 
 const getAmazingThings = () => {
-    return `Some amazing things with ${iAmUiLibrary()} and tiydings pr 3-1`;
+    return `Some amazing thing with ${iAmUiLibrary()}`;
 };
 
 module.exports = {
